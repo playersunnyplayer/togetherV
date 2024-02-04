7571b6a0ac4358639521ac5a371061f1cc044a60
Sunny kumar <playersunnyplayer@gmail.com>
2024-02-05 00:56:22 +0530

    addon popover

--- js/pdp.js
+++ js/pdp.js
@@ -75,3 +75,17 @@ $(".leftImgThumbs img").click(function() {
     $("#mainImg").attr("src", imageName);
     $("#fancyboxlink").attr("href", imageName);
 })
+
+$(function(){
+    $("[data-toggle=popover]").popover({
+        html : true,
+        content: function() {
+          var content = $(this).attr("data-popover-content");
+          return $(content).children(".popover-body").html();
+        },
+        title: function() {
+          var title = $(this).attr("data-popover-content");
+          return $(title).children(".popover-heading").html();
+        }
+    });
+});
