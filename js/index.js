8d34d08e0d6c43abfcfc3472b133ec58287ed8d3
playersunnyplayer <playersunnyplayer@gmail.com>
2024-03-09 15:29:00 +0530

    checkout page

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$('.homeBanner').owlCarousel({
+    margin: 40,
+    loop: true,
+    responsiveClass: true,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        }
+    }
+})
+
+$('.homeCategories').owlCarousel({
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    items: 6
+})
